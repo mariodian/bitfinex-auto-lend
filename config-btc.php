c7203cc2da8175d1387d6de29ff8ecaceee9a12e
Mario Dian <mariodian@gmail.com>
2017-04-01 10:44:02 +0800

    Adjust currencies swap limits to the current market situation

--- config-btc.php
+++ config-btc.php
@@ -5,10 +5,8 @@ include_once('./config.php');
 $config['currency'] = 'BTC'; // Currency to lend
 $config['period'] = 2; // Number of days to lend
 $config['remove_after'] = 50; // Minutes after unexecuted offer gets cancelled
-$config['max_total_swaps'] = 50; // Max number of total swaps to check for a closest rate *
+$config['max_total_swaps'] = 300; // Max number of total swaps to check for a closest rate *
 
 // * If there are e.g. total of 40 swaps at 1%/day and 60
 // swaps at 1.001%/day the script chooses the lower rate, because
 // we don't want to go beyond the rate of $config['max_total_swaps'] swaps.
-
-?>
--- config-usd.php
+++ config-usd.php
@@ -5,10 +5,9 @@ include_once('./config.php');
 $config['currency'] = 'USD'; // Currency to lend
 $config['period'] = 2; // Number of days to lend
 $config['remove_after'] = 50; // Minutes after unexecuted offer gets cancelled
-$config['max_total_swaps'] = 50000; // Max number of total swaps to check for a closest rate *
+$config['max_total_swaps'] = 100000; // Max number of total swaps to check for a closest rate *
 
-// * If there are e.g. total of 12000 swaps at 1%/day and 22000
+// * If there are e.g. total of 89000 swaps at 1%/day and 122000
 // swaps at 1.001%/day the script chooses the lower rate, because
 // we don't want to go beyond the rate of $config['max_total_swaps'] swaps.
-
-?>
+// If the currency doesn't get lent within an hour you may want to lower this value
