4082e533c7e34f6c9fc395704f442e055d630619
Mario Dian <mariodian@gmail.com>
2017-04-01 10:44:18 +0800

    Add new currencies

---
+++ config-zec.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once('./config.php');
+
+$config['currency'] = 'ZEC'; // Currency to lend
+$config['period'] = 2; // Number of days to lend
+$config['remove_after'] = 50; // Minutes after unexecuted offer gets cancelled
+$config['max_total_swaps'] = 100; // Max number of total swaps to check for a closest rate *
+
+// * If there are e.g. total of 80 swaps at 1%/day and 120
+// swaps at 1.001%/day the script chooses the lower rate, because
+// we don't want to go beyond the rate of $config['max_total_swaps'] swaps.
+// If the currency doesn't get lent within an hour you may want to lower this value
